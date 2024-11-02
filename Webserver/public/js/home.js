05702bd835d20fa1f245f67ea65327529217f892
Asger <asgerveng1@gmail.com>
2024-11-02 17:51:01 +0100

    update

--- Webserver/public/js/home.js
+++ Webserver/public/js/home.js
@@ -12,3 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch((error) => console.error("Error fetching data:", error));
 });
+
+document.getElementById("dropdown").addEventListener("change", function () {
+  var submitButton = document.getElementById("add_button");
+  if (this.value) {
+    submitButton.style.display = "block"; // Show the button when a valid option is selected
+  } else {
+    submitButton.style.display = "none"; // Hide the button if no valid option is selected
+  }
+});
